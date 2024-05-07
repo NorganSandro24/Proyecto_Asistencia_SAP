ca110d529d0fa9a7ce6e7129279af6b96b27cf31
NorganSandro24 <174439@unsaac.edu.pe>
2024-05-07 15:15:12 -0500

    comrntarios asgi.py de djangocrud

--- djangocrud/asgi.py
+++ djangocrud/asgi.py
@@ -1,16 +1,15 @@
-"""
-ASGI config for djangocrud project.
+"""El archivo asgi.py en un proyecto Django sirve como punto de entrada para las aplicaciones ASGI 
+(Asynchronous Server Gateway Interface). ASGI es una especificación para servidores web y aplicaciones
+web en Python que necesitan soportar operaciones asíncronas, como WebSockets."""
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
+#Importa el módulo os que proporciona funciones para interactuar con el sistema operativo.
 import os
-
+# Importa la función get_asgi_application del módulo asgi en el paquete django.core que se
+# utiliza para obtener la aplicación ASGI configurada para este proyecto Django.
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocrud.settings')
+# Configuración del módulo de configuración de Django para este proyecto
 
 application = get_asgi_application()
+# Obtener la aplicación ASGI para este proyecto Django
