4638805d1071ef5710a5f84bf0adbf24c541f511
NorganSandro24 <174439@unsaac.edu.pe>
2024-05-07 15:30:51 -0500

    comentarios en tests.py de templates

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
+# Importa la clase TestCase del módulo django.test, que se utiliza para escribir pruebas en Django
 
-# Create your tests here.
+# Aquí es donde se pueden definir las pruebas para tu aplicación Django. Las pruebas se definen como
+# métodos dentro de la clase de prueba (que hereda de TestCase).
