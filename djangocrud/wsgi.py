56131bfa3293e8bc89fef30382a136c255d543b4
NorganSandro24 <174439@unsaac.edu.pe>
2024-05-07 15:17:58 -0500

    comentarios wsgi.py de djangocrud

--- djangocrud/wsgi.py
+++ djangocrud/wsgi.py
@@ -1,16 +1,15 @@
 """
-WSGI config for djangocrud project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+En el archivo wsgi.py de un proyecto Django, el código que proporcionaste se encarga
+de configurar y devolver la aplicación WSGI (Web Server Gateway Interface) para el proyecto
 """
-
 import os
+# Importa el módulo os, que proporciona funciones para interactuar con el sistema operativo.
 
 from django.core.wsgi import get_wsgi_application
+# Importa la función get_wsgi_application del módulo wsgi en el paquete django.core, que se utiliza para obtener la aplicación WSGI configurada para este proyecto Django.
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocrud.settings')
+# Establece la variable de entorno DJANGO_SETTINGS_MODULE en 'djangocrud.settings', lo que le dice a Django qué archivo de configuración de settings debe usar.
 
 application = get_wsgi_application()
+# Obtén la aplicación WSGI para este proyecto Django, que está configurada según el archivo settings especificado.
