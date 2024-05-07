e84a5931d8598ee5a3edb2738bfb340107d9a187
NorganSandro24 <174439@unsaac.edu.pe>
2024-05-07 15:24:34 -0500

    comentarios apps.py de templates

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+# Importa la clase base AppConfig del módulo django.apps
 
 class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    # Define el campo predeterminado para la clave primaria como BigAutoField
     name = 'tasks'
+    # Especifica el nombre de la aplicación, en este caso, 'tasks'
