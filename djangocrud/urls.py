e74a8d4b6e40eb875474e3e1fa38e189798bdc36
NorganSandro24 <174439@unsaac.edu.pe>
2024-05-07 15:08:37 -0500

    comentarios urls.py de djangocrud

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -1,32 +1,28 @@
-"""djangocrud URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from tasks import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # Ruta para la página de inicio del sitio
     path('admin/', admin.site.urls),
+    # Ruta para acceder al panel de administración de Django
     path('signup/', views.signup, name='signup'),
+    # Ruta para la página de registro de usuarios
     path('tasks/', views.tasks, name='tasks'),
+    # Ruta para la página de listado de tareas
     path('tasks_completed/', views.tasks_completed, name='tasks_completed'),
+    # Ruta para la página de listado de tareas completadas
     path('logout/', views.signout, name='logout'),
+    # Ruta para cerrar sesión de un usuario
     path('signin/', views.signin, name='signin'),
+    # Ruta para la página de inicio de sesión
     path('create_task/', views.create_task, name='create_task'),
+    # Ruta para la página de creación de una nueva tarea
     path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
+    # Ruta para ver los detalles de una tarea específica
     path('taks/<int:task_id>/complete', views.complete_task, name='complete_task'),
+    # Ruta para marcar una tarea como completada
     path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
+    # Ruta para eliminar una tarea
 ]
