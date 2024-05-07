d30e6b788bdccc808dd7b0a7acce2b93ace0edd2
NorganSandro24 <174439@unsaac.edu.pe>
2024-05-07 15:23:11 -0500

    comentarios admin.py de templates

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+# Importa el módulo de administración de Django
 from .models import Task
-
-# Register your models here.
+# Importa el modelo Task desde el archivo models.py de la misma aplicación
 class TaskAdmin(admin.ModelAdmin):
-  readonly_fields = ('created', )
+    readonly_fields = ('created', )
+    # Define una clase TaskAdmin que hereda de admin.ModelAdmin y configura el campo 
+    # 'created' como de solo lectura en el panel de administración
 
 admin.site.register(Task, TaskAdmin)
+# Registra el modelo Task en el panel de administración de 
+# Django utilizando la clase TaskAdmin para personalizar su visualización y comportamiento en el panel
